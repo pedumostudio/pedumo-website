7250c9ee4e74d8e15a7a7cf04ffbc8644f7e0e15
Balogun Adeolu <pedumostudio@gmail.com>
2026-07-20 09:02:40 -0700

    Remove obsolete webpack config

--- next.config.ts
+++ next.config.ts
@@ -27,31 +27,13 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.worker\.ts$/,
-      type: "asset/resource",
-      generator: {
-        filename: "static/workers/[name].[hash][ext]",
-      },
-    });
-
-    return config;
-  },
-
   async headers() {
     return [
       {
         source: "/(.*)",
         headers: [
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
           {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
